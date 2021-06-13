882f283329632043373ec58d73452455dbe7d54a
Lawal Hassan <lawalhassan@Lawals-MacBook-Pro-2.local>
2021-06-13 01:33:30 +0100

    Auth settings and env variables - 1

--- projects/capstone/starter/auth.py
+++ projects/capstone/starter/auth.py
@@ -6,8 +6,6 @@ from urllib.request import urlopen
 from dotenv import load_dotenv
 import os
 
-# load_dotenv()
-
 
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
 ALGORITHMS = os.environ.get("ALGORITHMS")
